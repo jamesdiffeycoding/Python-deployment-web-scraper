a94cc8287c7f07e97f6507836bff3f7dc0d22fc6
James <jamesdiffey8@gmail.com>
2024-02-19 16:06:29 +0000

    test fetch of quotes

--- portfoliochecker/main.py
+++ portfoliochecker/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+import requests
 
 # Define the home page
 
@@ -9,4 +10,22 @@ def home():
 
 @app.route('/portfolio')
 def portfolio():
+    try:
+        page_to_scrape = requests.get("http://quotes.toscrape.com")
+        page_to_scrape.raise_for_status()  # Raise an HTTPError if the request was unsuccessful
+        soup = BeautifulSoup(page_to_scrape.content, 'html.parser')
+
+        # Find all quotes
+        quotes = soup.findAll("span", attrs={"class": "text"})
+
+        # Find all authors
+        authors = soup.findAll("small", attrs={"class": "author"})
+
+        # Combine quotes and authors into a list of tuples
+        quote_data = [(quote.text, author.text) for quote, author in zip(quotes, authors)]
+
+        return render_template('index.html', quotes=quote_data)
+
+    except requests.exceptions.RequestException as e:
+        return "Error making request: {}".format(e)
     return render_template('index.html')
