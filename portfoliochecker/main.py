08da7d6c508df3371ef09e52982762bd08fc133e
James <jamesdiffey8@gmail.com>
2024-02-19 16:22:34 +0000

    test shelter fetch and return

--- portfoliochecker/main.py
+++ portfoliochecker/main.py
@@ -32,21 +32,32 @@ def quotesworking():
 
 @app.route('/mydeployments')
 def mydeployments():
-    try:
-        page_to_scrape = requests.get("http://quotes.toscrape.com")
-        page_to_scrape.raise_for_status()  # Raise an HTTPError if the request was unsuccessful
-        soup = BeautifulSoup(page_to_scrape.content, 'html.parser')
+    count_of_working_sites = 0
+    count_of_potentially_broken_sites = 0 
+    try: 
+        shelterdatabase_page_to_scrape = requests.get("https://secure-nextjs-homeless-shelter-database.vercel.app/")
+        soup = BeautifulSoup(shelterdatabase_page_to_scrape.content, 'html.parser')
+        shelterdatabase_first_touch_points = soup.findAll("h1", attrs={"class": "white-font"})
+        shelterdatabase_second_touch_points = soup.findAll("strong")
 
-        # Find all quotes
-        quotes = soup.findAll("span", attrs={"class": "text"})
-
-        # Find all authors
-        authors = soup.findAll("small", attrs={"class": "author"})
-
-        # Combine quotes and authors into a list of tuples
-        quote_data = [(quote.text, author.text) for quote, author in zip(quotes, authors)]
-
-        return render_template('deployments.html', quotes=quote_data)
-
+        # Check if both lists are non-empty before proceeding
+        if shelterdatabase_first_touch_points and shelterdatabase_second_touch_points:
+            for shelterdatabase_first_touch_point, shelterdatabase_second_touch_point in zip(shelterdatabase_first_touch_points, shelterdatabase_second_touch_points):
+                print(shelterdatabase_first_touch_point.text)
+                print(shelterdatabase_second_touch_point.text)
+            shelterdatabase_status = 'True'
+        else:
+            print("No shelterdatabase_first_touch_points or shelterdatabase_second_touch_points found.")
+            shelterdatabase_status = 'False'
+        if shelterdatabase_status == 'True':
+            print('shelterdatabase is up and running and specified touch points were found')
+            count_of_working_sites += 1
+        else: 
+            print('The request went through suggesting the URL was valid, but the touch points you set may have changed')
+            count_of_potentially_broken_sites += 1
+        return{"4count_of_working_sites": count_of_working_sites, "count_of_potentially_broken_sites": count_of_potentially_broken_sites}
     except requests.exceptions.RequestException as e:
-        return "Error making request: {}".format(e)
+        print("Error making request to shelterdatabase. Maybe there was a typo?") 
+        # print(e)
+        count_of_potentially_broken_sites += 1
+        return{"5count_of_working_sites": count_of_working_sites, "count_of_potentially_broken_sites": count_of_potentially_broken_sites}
