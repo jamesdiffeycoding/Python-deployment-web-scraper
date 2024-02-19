e0ce6dc6e13d2d981370bb179c2abdeabed1658a
James <jamesdiffey8@gmail.com>
2024-02-19 16:15:56 +0000

    creating new test route

--- portfoliochecker/main.py
+++ portfoliochecker/main.py
@@ -24,7 +24,29 @@ def quotesworking():
         # Combine quotes and authors into a list of tuples
         quote_data = [(quote.text, author.text) for quote, author in zip(quotes, authors)]
 
-        return render_template('index.html', quotes=quote_data)
+        return render_template('quotes.html', quotes=quote_data)
+
+    except requests.exceptions.RequestException as e:
+        return "Error making request: {}".format(e)
+
+
+@app.route('/mydeployments')
+def mydeployments():
+    try:
+        page_to_scrape = requests.get("http://quotes.toscrape.com")
+        page_to_scrape.raise_for_status()  # Raise an HTTPError if the request was unsuccessful
+        soup = BeautifulSoup(page_to_scrape.content, 'html.parser')
+
+        # Find all quotes
+        quotes = soup.findAll("span", attrs={"class": "text"})
+
+        # Find all authors
+        authors = soup.findAll("small", attrs={"class": "author"})
+
+        # Combine quotes and authors into a list of tuples
+        quote_data = [(quote.text, author.text) for quote, author in zip(quotes, authors)]
+
+        return render_template('deployments.html', quotes=quote_data)
 
     except requests.exceptions.RequestException as e:
         return "Error making request: {}".format(e)
