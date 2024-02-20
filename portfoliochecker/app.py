1c845f25346b2d882e1221fa5c425249e0143ed1
James <jamesdiffey8@gmail.com>
2024-02-20 10:47:40 +0000

    test update again

--- portfoliochecker/app.py
+++ portfoliochecker/app.py
@@ -121,8 +121,8 @@ def home():
     try: 
         page_to_scrape = requests.get("https://developer-lessons-react.vercel.app/")
         soup = BeautifulSoup(page_to_scrape.content, 'html.parser')
-        first_touch_points = soup.findAll("div", attrs={"class": "settings-container"})
-        second_touch_points = soup.findAll("p")
+        first_touch_points = soup.findAll("section", attrs={"class": "contents-container"})
+        second_touch_points = soup.findAll("div")
         page_to_scrape.raise_for_status()  # Raise an exception for HTTP errors
 
         # Check if both lists are non-empty before proceeding
